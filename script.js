e64fc1d76ce493c878c65d6050c59fca45adad53
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-11 18:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	Car(make,model){
+	this.make = make;
+	this.model = model;
+	}
 
-function SportsCar(make, model, topSpeed) {}
+	getMakeModel(){
+		return `${make} ${model}`;
+	}
+}
+
+function SportsCar(make, model, topSpeed) extends Car {
+	SportsCar(make,model,topSpeed){
+		super(make,model);
+		this.make = make;
+		this.model = model;
+		this.topSpeed = topSpeed;
+	}
+	getTpSpeed(){
+		return topSpeed;
+	}
+}
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed());
 
 // Do not change the code below
 window.Car = Car;
